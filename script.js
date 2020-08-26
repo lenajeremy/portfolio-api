e0be0c72e5d7025930c1be55a7a11f21d972deff
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-08-26 08:13:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function(){
+	let theme = localStorage.getItem('theme')
+	if(theme == null){
+		setTheme('light')
+	}else{
+		setTheme(theme)
+	}
+})
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'purple.css'
+	}
+	if(mode == 'pink'){
+		document.getElementById('theme-style').href = 'pink.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
+
+document.querySelectorAll('.input-field').forEach(input => input.addEventListener('focusin', ()=>{
+	input.nextElementSibling.style.width = '100%'
+}))
+document.querySelectorAll('.input-field').forEach(input => input.addEventListener('focusout', ()=>{
+	input.nextElementSibling.style.width = '0%'
+}))
