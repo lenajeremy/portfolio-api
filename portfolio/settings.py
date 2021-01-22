5999b974b11362f305f9d65ba63daee744bc98d0
Jeremiah Lena <jeremiahlena13@gmail.com>
2021-01-22 01:37:28 +0100

    adddded stuff to allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'jacinth-porfolio.herokuapp']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
