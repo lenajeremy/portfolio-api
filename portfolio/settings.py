c295672384b11419843df5f1253d2e993cd5560a
Lena Jeremiah <49532117+Jeremiahjacinth13@users.noreply.github.com>
2021-12-06 21:57:48 +0100

    set up for production

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', '.vercel.app']
 
 
 # Application definition
