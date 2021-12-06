db2661ee80a141cb16eb08f2570d13b2a2eefb92
Lena Jeremiah <49532117+Jeremiahjacinth13@users.noreply.github.com>
2021-12-06 17:23:24 +0100

    new changes

--- port/urls.py
+++ port/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name = 'homepage'),
     path('new/', views.new_project, name = 'new_project'),
     path('contact/', views.contact, name = 'contact'),
-    path('/download_resume', views.download_resume, name = 'download_resume'),
+    path('download_resume/', views.download_resume, name = 'download_resume'),
     path('<str:project>/', views.project, name = 'project')
 ]
 
